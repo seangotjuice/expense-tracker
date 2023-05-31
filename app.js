c18ce781c08165c908404f6b84fb549ad9b171d7
Sean Chen <seanchen112@gmail.com>
2023-05-31 17:35:08 +0800

    mongoose 建立連線

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+require("./config/mongoose"); // mongoose 連線
+const Expense = require("./models/expense");
 
 const app = express();
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  //   res.send(`This is my first Express Web App`);
   res.render("index");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(3000, () => {
---
+++ models/expense.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const expenseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  expense: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Expense", expenseSchema);
