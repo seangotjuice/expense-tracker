11ced985a9dca3920fc90b5ab4888ed5ba3fb741
Sean Chen <seanchen112@gmail.com>
2023-06-11 20:57:27 +0800

    feat: filter by category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,7 +6,10 @@ const Category = require("../../models/category");
 // 瀏覽全部（原本我寫的）
 router.get("/", (req, res) => {
   const userId = req.user._id;
-  return Expense.find({ userId })
+  const categoryId = req.query.categoryId; // 從查詢參數中獲取 categoryId
+  const filter = categoryId ? { userId, categoryId: categoryId } : { userId };
+
+  return Expense.find(filter)
     .populate("categoryId")
     .lean()
     .then((exp) => {
@@ -22,19 +25,19 @@ router.get("/", (req, res) => {
           expense.date
         );
       });
-      // // 取得 Category 資料
-      // return Category.find()
-      //   .lean()
-      //   .then((categories) => {
-      //     // 傳遞 exp 和 categories 給模板引擎進行渲染
-      //     console.log("首頁exp:", exp);
-      //     console.log("首頁total:", total);
-      //     console.log("categories:", categories);
-      //     res.render("index", { exp, total, categories });
-      //   })
-      //   .catch((err) => console.log(err));
+      // 取得 Category 資料
+      return Category.find()
+        .lean()
+        .then((categories) => {
+          // 傳遞 exp 和 categories 給模板引擎進行渲染
+          console.log("首頁exp:", exp);
+          console.log("首頁total:", total);
+          console.log("categories:", categories);
+          res.render("index", { exp, total, categories });
+        })
+        .catch((err) => console.log(err));
 
-      res.render("index", { exp, total });
+      // res.render("index", { exp, total });
     })
     .catch((err) => console.log(err));
 });
